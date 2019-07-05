00afa7b1d04d1c0ecb191f1b807e3f2fee5aee39
vithyasri <52270841+vithyasri@users.noreply.github.com>
2019-07-05 14:48:25 +0530

    Create vithyay.py

---
+++ vithyay.py
@@ -0,0 +1,11 @@
+def minimum(a, n): 
+ 
+ 
+    minpos = a.index(min(a)) 
+ 
+ 
+    maxpos = a.index(max(a))  
+ 
+ 
+    print "The maximum is at position", maxpos + 1  
+    print "The minimum is at position", minpos + 1
